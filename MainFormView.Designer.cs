b5c4cd5899b903ed42acf94894193264951e36a2
Tiikeri1 <uskeliini@gmail.com>
2026-03-19 11:37:22 +0200

    Add project files.

---
+++ MainFormView.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace VillageNewbies_Projekti
+{
+    partial class MainFormView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelTop = new Panel();
+            label1 = new Label();
+            panelLeft = new Panel();
+            btnRaportit = new Button();
+            btnLaskut = new Button();
+            btnVaraukset = new Button();
+            btnAsiakkaat = new Button();
+            btnPalvelut = new Button();
+            btnMokit = new Button();
+            btnAlueet = new Button();
+            btnEtusivu = new Button();
+            panelFill = new Panel();
+            panelTop.SuspendLayout();
+            panelLeft.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.BackColor = SystemColors.Control;
+            panelTop.Controls.Add(label1);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Size = new Size(1284, 70);
+            panelTop.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(10, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(440, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Village Newbies - Varausjärjestelmä";
+            // 
+            // panelLeft
+            // 
+            panelLeft.AccessibleRole = AccessibleRole.None;
+            panelLeft.Controls.Add(btnRaportit);
+            panelLeft.Controls.Add(btnLaskut);
+            panelLeft.Controls.Add(btnVaraukset);
+            panelLeft.Controls.Add(btnAsiakkaat);
+            panelLeft.Controls.Add(btnPalvelut);
+            panelLeft.Controls.Add(btnMokit);
+            panelLeft.Controls.Add(btnAlueet);
+            panelLeft.Controls.Add(btnEtusivu);
+            panelLeft.Dock = DockStyle.Left;
+            panelLeft.Location = new Point(0, 70);
+            panelLeft.Name = "panelLeft";
+            panelLeft.Size = new Size(220, 680);
+            panelLeft.TabIndex = 1;
+            // 
+            // btnRaportit
+            // 
+            btnRaportit.Dock = DockStyle.Top;
+            btnRaportit.FlatStyle = FlatStyle.Flat;
+            btnRaportit.Location = new Point(0, 595);
+            btnRaportit.Name = "btnRaportit";
+            btnRaportit.Size = new Size(220, 85);
+            btnRaportit.TabIndex = 7;
+            btnRaportit.Text = "Raportit";
+            btnRaportit.UseVisualStyleBackColor = true;
+            btnRaportit.Click += btnRaportit_Click;
+            // 
+            // btnLaskut
+            // 
+            btnLaskut.Dock = DockStyle.Top;
+            btnLaskut.FlatStyle = FlatStyle.Flat;
+            btnLaskut.Location = new Point(0, 510);
+            btnLaskut.Name = "btnLaskut";
+            btnLaskut.Size = new Size(220, 85);
+            btnLaskut.TabIndex = 6;
+            btnLaskut.Text = "Laskut";
+            btnLaskut.UseVisualStyleBackColor = true;
+            btnLaskut.Click += btnLaskut_Click;
+            // 
+            // btnVaraukset
+            // 
+            btnVaraukset.Dock = DockStyle.Top;
+            btnVaraukset.FlatStyle = FlatStyle.Flat;
+            btnVaraukset.Location = new Point(0, 425);
+            btnVaraukset.Name = "btnVaraukset";
+            btnVaraukset.Size = new Size(220, 85);
+            btnVaraukset.TabIndex = 5;
+            btnVaraukset.Text = "Varaukset";
+            btnVaraukset.UseVisualStyleBackColor = true;
+            btnVaraukset.Click += btnVaraukset_Click;
+            // 
+            // btnAsiakkaat
+            // 
+            btnAsiakkaat.Dock = DockStyle.Top;
+            btnAsiakkaat.FlatStyle = FlatStyle.Flat;
+            btnAsiakkaat.Location = new Point(0, 340);
+            btnAsiakkaat.Name = "btnAsiakkaat";
+            btnAsiakkaat.Size = new Size(220, 85);
+            btnAsiakkaat.TabIndex = 4;
+            btnAsiakkaat.Text = "Asiakkaat";
+            btnAsiakkaat.UseVisualStyleBackColor = true;
+            btnAsiakkaat.Click += btnAsiakkaat_Click;
+            // 
+            // btnPalvelut
+            // 
+            btnPalvelut.Dock = DockStyle.Top;
+            btnPalvelut.FlatStyle = FlatStyle.Flat;
+            btnPalvelut.Location = new Point(0, 255);
+            btnPalvelut.Name = "btnPalvelut";
+            btnPalvelut.Size = new Size(220, 85);
+            btnPalvelut.TabIndex = 3;
+            btnPalvelut.Text = "Palvelut";
+            btnPalvelut.UseVisualStyleBackColor = true;
+            btnPalvelut.Click += btnPalvelut_Click;
+            // 
+            // btnMokit
+            // 
+            btnMokit.Dock = DockStyle.Top;
+            btnMokit.FlatStyle = FlatStyle.Flat;
+            btnMokit.Location = new Point(0, 170);
+            btnMokit.Name = "btnMokit";
+            btnMokit.Size = new Size(220, 85);
+            btnMokit.TabIndex = 2;
+            btnMokit.Text = "Mökit";
+            btnMokit.UseVisualStyleBackColor = true;
+            btnMokit.Click += btnMokit_Click;
+            // 
+            // btnAlueet
+            // 
+            btnAlueet.Dock = DockStyle.Top;
+            btnAlueet.FlatStyle = FlatStyle.Flat;
+            btnAlueet.Location = new Point(0, 85);
+            btnAlueet.Name = "btnAlueet";
+            btnAlueet.Size = new Size(220, 85);
+            btnAlueet.TabIndex = 1;
+            btnAlueet.Text = "Alueet";
+            btnAlueet.UseVisualStyleBackColor = true;
+            btnAlueet.Click += btnAlueet_Click;
+            // 
+            // btnEtusivu
+            // 
+            btnEtusivu.Dock = DockStyle.Top;
+            btnEtusivu.FlatStyle = FlatStyle.Flat;
+            btnEtusivu.Location = new Point(0, 0);
+            btnEtusivu.Name = "btnEtusivu";
+            btnEtusivu.Size = new Size(220, 85);
+            btnEtusivu.TabIndex = 0;
+            btnEtusivu.Text = "Etusivu";
+            btnEtusivu.UseVisualStyleBackColor = true;
+            btnEtusivu.Click += btnEtusivu_Click;
+            // 
+            // panelFill
+            // 
+            panelFill.Dock = DockStyle.Fill;
+            panelFill.Location = new Point(220, 70);
+            panelFill.Name = "panelFill";
+            panelFill.Size = new Size(1064, 680);
+            panelFill.TabIndex = 2;
+            // 
+            // MainFormView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1284, 750);
+            Controls.Add(panelFill);
+            Controls.Add(panelLeft);
+            Controls.Add(panelTop);
+            MaximumSize = new Size(1300, 789);
+            MinimumSize = new Size(1100, 789);
+            Name = "MainFormView";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainFormView";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            panelLeft.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private Panel panelLeft;
+        private Panel panelFill;
+        private Button btnRaportit;
+        private Button btnLaskut;
+        private Button btnVaraukset;
+        private Button btnAsiakkaat;
+        private Button btnPalvelut;
+        private Button btnMokit;
+        private Button btnAlueet;
+        private Button btnEtusivu;
+        private Label label1;
+    }
+}
---
+++ MainFormView.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VillageNewbies_Projekti.Views;
+
+namespace VillageNewbies_Projekti
+{
+    public partial class MainFormView : Form
+    {
+        public MainFormView()
+        {
+            InitializeComponent();
+            // Alussa näytetään panelissa etusivu
+            ShowView(new EtusivuView());
+        }
+
+        // Apumetodi joka vaihtaa näkymän
+        private void ShowView(UserControl view)
+        {
+            // Tyhjennetään panelFill kaikista sen sisällä olevista kontrolleista - Clear();
+            panelFill.Controls.Clear();
+            // Asetetaan uusi view täyttämään koko paneelin - DockStyle.Fill
+            view.Dock = DockStyle.Fill;
+            // Lisätään uusi view panelFill:iin - Add(view);
+            panelFill.Controls.Add(view);
+        }
+
+        // Jokaiselle napille oma tapahtuma
+        private void btnEtusivu_Click(object sender, EventArgs e)
+        {
+            ShowView(new EtusivuView());
+        }
+
+        private void btnAlueet_Click(object sender, EventArgs e)
+        {
+            ShowView(new AlueetView());
+        }
+
+        private void btnMokit_Click(object sender, EventArgs e)
+        {
+            ShowView(new MokitView());
+        }
+
+        private void btnPalvelut_Click(object sender, EventArgs e)
+        {
+            ShowView(new PalvelutView());
+        }
+
+        private void btnAsiakkaat_Click(object sender, EventArgs e)
+        {
+            ShowView(new AsiakkaatView());
+        }
+
+        private void btnVaraukset_Click(object sender, EventArgs e)
+        {
+            ShowView(new VarauksetView());
+        }
+
+        private void btnLaskut_Click(object sender, EventArgs e)
+        {
+            ShowView(new LaskutView());
+        }
+
+        private void btnRaportit_Click(object sender, EventArgs e)
+        {
+            ShowView(new RaportitView());
+        }
+    }
+}
