fb201eede4a442a2d84b9ef62e14a81e9d8a9cec
Tiikeri1 <uskeliini@gmail.com>
2026-03-23 13:07:37 +0200

    Tietokantayhteys lisätty.

---
+++ Database/TietokantaYhteys.cs
@@ -0,0 +1,25 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VillageNewbies_Projekti.Database
+{
+    public class TietokantaYhteys
+    {
+        /*
+         * MUUTA SALASANAT JA KÄYTTÄJÄT OMIISI ENNEN AJAMISTA.
+         * MUISTA MYÖS JOS PUSHAAT GITHUBIIN NIIN NÄMÄ ASETTAA NÄMÄ.
+         * DATABASEN NIMEÄ EI TARVITSE MUUTTAA.
+        */
+        private string connectionString = "Server=localhost;Port=3306;Database=vn;User=root;Password=omasalasanasi";
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
