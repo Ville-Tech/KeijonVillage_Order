7370c736cefd509012d6d6a0d7126ef6724a1066
Tiikeri1 <uskeliini@gmail.com>
2026-03-23 12:01:05 +0200

    Tehty Asiakas.cs ja Mokki.cs

---
+++ Services/Asiakas.cs
@@ -0,0 +1,58 @@
+﻿namespace VillageNewbies_Projekti.Services
+{
+    // Asiakas luokka
+    public class Asiakas
+    {
+        // Kysymysmerkit meinaa, että value voi olla null.
+        private int id;
+        private string? etunimi;
+        private string? sukunimi;
+        private string? lahiosoite;
+        private string postinro;
+        private string? sahkoposti;
+        private string? puhelin;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string? Etunimi
+        {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
+        public string? Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
+        public string? Lahiosoite
+        {
+            get { return lahiosoite; }
+            set { lahiosoite = value; }
+        }
+        public string Postinro
+        {
+            get { return postinro; }
+            // Asetetaan postinro vain jos se on 5 merkkiä pitkä, sekä merkit ovat numeroita.
+            set
+            {
+                if (value.Length == 5 && value.All(char.IsDigit))
+                {
+                    postinro = value;
+                }
+            }
+        }
+        public string? Sahkoposti
+        {
+            get { return sahkoposti; }
+            set { sahkoposti = value; }
+        }
+        public string? Puhelin
+        {
+            get { return puhelin; }
+            set { puhelin = value; }
+        }
+    }
+}
---
+++ Services/Mokki.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillageNewbies_Projekti.Services
+{
+    public class Mokki
+    {
+        // Kysymysmerkit meinaa, että value voi olla null.
+        private int id;
+        private int alue_id;
+        private string postinro;
+        private string? mokkinimi;
+        private string? katuosoite;
+        private double hinta;
+        private string? kuvaus;
+        private int henkilomaara;
+        private string? varustelu;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int Alue_ID
+        {
+            get { return alue_id; }
+            set { alue_id = value; }
+        }
+        public string Postinro
+        {
+            get { return postinro; }
+            set
+            {
+                if (value.Length == 5 && value.All(char.IsDigit))
+                {
+                    postinro = value;
+                }
+            }
+        }
+        public string? Mokkinimi
+        {
+            get { return mokkinimi; }
+            set { mokkinimi = value; }
+        }
+        public string? Katuosoite
+        {
+            get { return katuosoite; }
+            set { katuosoite = value; }
+        }
+        public double Hinta
+        {
+            get { return hinta; }
+            set 
+            {
+                if(value >= 0)
+                {
+                    hinta = value;
+                }
+            }
+        }
+        public string? Kuvaus
+        {
+            get { return kuvaus; }
+            set { kuvaus = value; }
+        }
+        public int Henkilomaara
+        {
+            get { return henkilomaara; }
+            set 
+            {
+                if (value >= 0)
+                {
+                    henkilomaara = value; 
+                }
+            }
+        }
+        public string? Varustelu
+        {
+            get { return varustelu; }
+            set { varustelu = value; }
+        }
+    }
+}
