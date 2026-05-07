8ddb1b9b281a08d86b1941cf2c33b91f19dca5ce
Ville-Tech <s2303973@edu.savonia.fi>
2026-05-07 11:08:52 +0300

    Update and rename mokit.sql to turha

--- Database/mokit.sql
+++
@@ -1,9 +0,0 @@
--- Postinumerot
-INSERT INTO posti (postinro, toimipaikka) VALUES
-('00100', 'Helsinki'),
-('00200', 'Espoo');
-
--- Mökit
-INSERT INTO mokki (mokkinimi, katuosoite, postinro, hinta) VALUES
-('Järvimökki', 'Rantatie 1', '00100', 120.00),
-('Metsämökki', 'Kuusitie 5', '00200', 95.00);
